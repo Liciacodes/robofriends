afcc73033d17e604d91538ec774b32bebe8e8e38
Felicia Udosen <udosenfelicia@gmail.com>
2022-03-26 11:29:47 +0100

    made changes to the card.js file

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({name, email, id}) => {
         <div className=' tc bg-light-green dib br3 pd3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?200X200`}/>
             <div>
-            <h2 helvetica>{name}</h2>
+            <h2>{name}</h2>
             <p>{email}</p>
             </div>
         </div>
